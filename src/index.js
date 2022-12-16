d2615a4bdf54df41cd9a9cd45ea9e7b3dfaa431e
santoshmondal <santosh.ece06@gmail.com>
2022-12-16 11:58:42 +0530

    messages.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,19 @@ const app = express();
 /*  http://localhost:3000/  */
 app.get("/", (req, res) => res.send("Hello!"));
 
-/*  http://localhost:3000/cdac  */
-app.get("/cdac", (req, res) => res.send("Hello 2!"));
+/*  http://localhost:3000/message  */
+app.get("/message", (req, res) => {
+  let message = { id: 1, message: "Hi", messageTime: new Date() };
+  res.json(message);
+});
 
-/*  http://localhost:3000/explore  */
-app.get("/explore", (req, res) => res.send("Hello 3!"));
+/*  http://localhost:3000/messages  */
+app.get("/messages/", (req, res) => {
+  let list = [];
+  list.push({ id: 1, message: "Hi", messageTime: new Date() });
+  list.push({ id: 2, message: "Hello", messageTime: new Date() });
+
+  res.json(list);
+});
 
 app.listen(3000);
