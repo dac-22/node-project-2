9db3975f599f30c8d1e66639409cb4a5420e7dfd
santoshmondal <santosh.ece06@gmail.com>
2022-12-16 11:48:24 +0530

    basic endpoints.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import express from "express";
 const app = express();
+
+/** Learning here Creating URLs / Endpoints */
+
+/*  http://localhost:3000/  */
+app.get("/", (req, res) => res.send("Hello!"));
+
+/*  http://localhost:3000/cdac  */
+app.get("/cdac", (req, res) => res.send("Hello 2!"));
+
+/*  http://localhost:3000/explore  */
+app.get("/explore", (req, res) => res.send("Hello 3!"));
+
 app.listen(3000);
