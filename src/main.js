91fabbda577d108ff38d1e8e73d4060d1992f4d9
santoshmondal <santosh.ece06@gmail.com>
2022-12-16 10:16:53 +0530

    basics of read and write.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createConnection } from "mysql";
 
-function main() {
+function insertMessage(message, reply = 0) {
   const connectionUri = {
     host: "localhost",
     user: "root",
@@ -11,13 +11,38 @@ function main() {
   const connection = createConnection(connectionUri);
   connection.connect();
 
-  let message = "Helloo How re are u";
-  let sql = `INSERT INTO message (message, reply) VALUES ('${message}', 0)`;
+  let sql = `INSERT INTO message (message, reply) VALUES ('${message}', ${reply})`;
   connection.query(sql);
 
   console.log("Record Addedd!");
 
   connection.end();
+}
+
+function readMessage() {
+  const connectionUri = {
+    host: "localhost",
+    user: "root",
+    password: "mysql",
+    database: "cdac",
+  };
+
+  const connection = createConnection(connectionUri);
+  connection.connect();
+
+  let sql = `SELECT * FROM message`;
+  connection.query(sql, (err, results) => {
+    // console.log(results);
+    return results;
+  });
+
+  connection.end();
+}
+
+function main() {
+  // insertMessage("Hello Delhi!!!");
+  let results = readMessage();
+  console.log(results);
 }
 
 main();
