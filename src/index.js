9ab27f487d6ed2e3d8ade9b1af90f38c6974c060
santoshmondal <santosh.ece06@gmail.com>
2022-12-16 12:23:02 +0530

    basic post. and tested using postman.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express();
 /*  http://localhost:3000/  */
 app.get("/", (req, res) => res.send("Hello!"));
 
+/* GET / GIVE ME MESSAGE */
 /*  http://localhost:3000/message  */
 app.get("/message", (req, res) => {
   let message = { id: 1, message: "Hi", messageTime: new Date() };
@@ -34,6 +35,31 @@ app.get("/messages/", async (req, res) => {
   await connection.endAsync();
 
   res.json(results);
+});
+
+/*  POST / INSERT / ADD / CREATE NEW MESSAGE */
+/*  http://localhost:3000/message  */
+app.post("/message", async (req, res) => {
+  let connectUri = {
+    host: "localhost",
+    user: "root",
+    password: "mysql",
+    database: "cdac",
+  };
+  let connection = createConnection(connectUri);
+  bluebird.promisifyAll(connection);
+
+  await connection.connectAsync();
+
+  let message = "Hello Node Mysql Bluebird!!";
+  let reply = 0;
+
+  let sql = `INSERT INTO message (message, reply) VALUES ('${message}', ${reply})`;
+  await connection.queryAsync(sql);
+
+  connection.endAsync();
+
+  res.json({ msg: "Record added!" });
 });
 
 app.listen(3000);
